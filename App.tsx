50840d7b0e49c4fff9c779746a7580e2835c767f
BlackMoonC <rakhel.sera@dataon.com>
2024-06-24 08:57:10 +0700

    api done

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import BottomTabNavigator from './src/navigations/BottomTabNavigation'
 
 export default function App(): JSX.Element {
+  useEffect(() => {
+
+  const saveToken = async (token) => {
+    try {
+      await Keychain.setGenericPassword('token', token);
+      console.log('Token saved successfully');
+    } catch (error) {
+      console.log('Could not save token', error);
+    }
+  };
+  saveToken();
+  },[])
   return (
     <NavigationContainer>
       <BottomTabNavigator />
--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import axios from 'axios'
 import { View, Text, Button } from 'react-native'
+const ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTM2ZjA1NzE4ODM2OTdkMWVkMmI4YmRjMjU3MzE0YSIsIm5iZiI6MTcxOTE5MTQyNi45MjI4MDcsInN1YiI6IjY2NzhjNWM4MWY3NjAxNTg1OWVkZjY0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AgKAzWEgQKH1s6dV6n5cYqiMX-1e2tWFS4EFnXzwVwk'
+const URL = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
 
-export default function MovieDetail({ navigation }): React.JSX.Element {
+type Props = {
+  navigation: any,
+}
+
+export default function MovieDetail({ navigation } : Props): React.JSX.Element {
+  
+  const fetchData = async() => {
+    try {
+      const Respon = await axios.get(URL , {headers: {'Accept': 'application/json','Authorization': `Bearer ${ACCESS_TOKEN}`}});
+      console.log("🚀 ~ fetchRespon ~ Respon:", Respon.data)
+    } catch (
+    err) {
+      console.log("🚀 ~ fetchData ~ err:", err) 
+    }
+  }
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ marginBottom: 8 }}>Movie Detail Page</Text>
       <Button
-        title="KEMBALI"
+        title="FETCH DATA"
         onPress={() => {
-          navigation.navigate('MovieScreen')
+          fetchData()
         }}
       />
     </View>
