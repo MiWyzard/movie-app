5409da2848d03951f67042dd99aa4f4930c7ccce
BlackMoonC <rakhel.sera@dataon.com>
2024-06-24 09:42:30 +0700

    set up environment variables

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import axios from 'axios'
 import { View, Text, Button } from 'react-native'
-const ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTM2ZjA1NzE4ODM2OTdkMWVkMmI4YmRjMjU3MzE0YSIsIm5iZiI6MTcxOTE5MTQyNi45MjI4MDcsInN1YiI6IjY2NzhjNWM4MWY3NjAxNTg1OWVkZjY0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AgKAzWEgQKH1s6dV6n5cYqiMX-1e2tWFS4EFnXzwVwk'
-const URL = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
+import  {API_URL, API_ACCESS_TOKEN} from '@env'
 
 type Props = {
   navigation: any,
 }
 
 export default function MovieDetail({ navigation } : Props): React.JSX.Element {
-  
+  if (API_ACCESS_TOKEN == null || API_URL == null) {
+    throw new Error('ENV not found')
+  }
   const fetchData = async() => {
     try {
-      const Respon = await axios.get(URL , {headers: {'Accept': 'application/json','Authorization': `Bearer ${ACCESS_TOKEN}`}});
+      const Respon = await axios.get(API_URL , {headers: {'Accept': 'application/json','Authorization': `Bearer ${API_ACCESS_TOKEN}`}});
       console.log("🚀 ~ fetchRespon ~ Respon:", Respon.data)
     } catch (
     err) {
