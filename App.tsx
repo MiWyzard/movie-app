fa54bbe9bd198ead3d0a49c5779ea14e21d0f99d
“rakhel.sera” <rakhel.sera@dataon.com>
2024-06-25 08:57:01 +0700

    proses development layout home done

--- App.tsx
+++ App.tsx
@@ -3,18 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import BottomTabNavigator from './src/navigations/BottomTabNavigation'
 
 export default function App(): JSX.Element {
-  useEffect(() => {
 
-  const saveToken = async (token) => {
-    try {
-      await Keychain.setGenericPassword('token', token);
-      console.log('Token saved successfully');
-    } catch (error) {
-      console.log('Could not save token', error);
-    }
-  };
-  saveToken();
-  },[])
   return (
     <NavigationContainer>
       <BottomTabNavigator />
